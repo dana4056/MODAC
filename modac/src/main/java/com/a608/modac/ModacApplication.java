7e3d331f473a4c1a903079d26ea864eff8d3b85f
dana <dana4056@naver.com>
2023-01-16 17:40:46 +0900

    Init: Create Project
    첫 프로젝트 생성

---
+++ modac/src/main/java/com/a608/modac/ModacApplication.java
@@ -0,0 +1,13 @@
+package com.a608.modac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModacApplication.class, args);
+    }
+
+}
---
+++ modac/src/test/java/com/a608/modac/ModacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a608.modac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
