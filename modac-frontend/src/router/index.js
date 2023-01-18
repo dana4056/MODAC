6b3b76fae7e976ad88fc3c00f8874e30991f2f32
dana4056 <dana4056@naver.com>
2023-01-19 01:05:29 +0900

    Feat: 커밋할 저장소 선택 기능, 파일 원격 커밋 기능
    커밋할 저장소 선택한 후 파일명, 커밋메시지 입력받아 자동 커밋되도록
    구현. 자동 커밋부분에서 테스트 실패

--- modac-frontend/src/router/index.js
+++ modac-frontend/src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import TodoListView from "../views/TodoListView.vue";
 import StudyRoomView from "../views/StudyRoomView.vue";
 
+import GithubTest from "../views/GithubTest.vue";
+import GithubTest2 from "../views/GithubTest2.vue";
+import GithubTest3 from "../views/GithubTest3.vue";
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,6 +20,21 @@ const router = createRouter({
       name: "studyRoom",
       component: StudyRoomView,
     },
+    {
+      path: "/before",
+      name: "before",
+      component: GithubTest,
+    },
+    {
+      path: "/after",
+      name: "after",
+      component: GithubTest2,
+    },
+    {
+      path: "/commit/:user/:repo",
+      name: "commit",
+      component: GithubTest3,
+    }
   ],
 });
 
