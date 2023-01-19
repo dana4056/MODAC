e7c6c84b3144a5b6c0edf1622513e58f2874defe
dana <dana4056@naver.com>
2023-01-19 17:52:13 +0900

    Feat: 깃허브 자동 커밋&푸시, 저장소 생성
    github API 이용해 페이지에서 데이터 자동 원격 커밋 기능
    github API 이용해 페이지에서 원격 저장소 자동 생성

--- modac-frontend/src/router/index.js
+++ modac-frontend/src/router/index.js
@@ -5,7 +5,9 @@ import ArticleView from "../views/ArticleView.vue";
 import TestView from "@/views/TestView.vue";
 import GithubTest from "../views/GithubTest.vue";
 import GithubTest2 from "../views/GithubTest2.vue";
+import GithubTest2_2 from "../views/GithubTest2-2.vue";
 import GithubTest3 from "../views/GithubTest3.vue";
+import GithubTest4 from "../views/GithubTest4.vue";
 
 
 const router = createRouter({
@@ -37,9 +39,19 @@ const router = createRouter({
       component: GithubTest2,
     },
     {
+      path: "/createRepo/:user",
+      name: "createRepo",
+      component: GithubTest2_2,
+    },
+    {
       path: "/commit/:user/:repo",
       name: "commit",
       component: GithubTest3,
+    },
+    {
+      path: "/repo/:user/:repo",
+      name: "repo",
+      component: GithubTest4,
     },
   ],
 });
