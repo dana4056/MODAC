a6f3eec3c03141aff7947daaf5e20d6967fc3304
권용재 <yongjae116@gmail.com>
2023-01-19 17:56:13 +0900

    Feat: Article Create 구현중
    Todo store에서 Article store로 데이터를 가져와 전처리 중

--- modac-frontend/src/domains/Article/ArticleList.vue
+++ modac-frontend/src/domains/Article/ArticleList.vue
@@ -21,10 +21,69 @@
 <script setup>
 import "./ArticleList.css"
 import ArticleListItem from "./ArticleListItem.vue"
-import { useTodoStore } from "../../stores/article";
+import { useArticleStore } from "../../stores/article";
+import { useTodoStore } from "../../stores/todo";
 import { ref, computed } from "vue"
-const store = useTodoStore();
+const todostore = useTodoStore();
+const articlestore = useArticleStore()
 const test = ref("")
+const payload = ref([])
+const todos = todostore.todos.filter(todo => todo.status === 2);
+
+
+
+
+const originalTodos = [{todos_seq: 1, status: 1}, {todos_seq: 1, status: 2}]
+
+// 1. filter 사용해서 각 객체의 status가 2인 것인 새로운 배열 만들기
+const filterTodos1 = todostore.todos.filter(todo => originalTodos.status === 2);
+
+const filterTodos2 = originalTodos.filter((elem) => { 
+    console.log(elem)
+    return elem.status === 2
+})
+console.log('filterTodos1', filterTodos1)
+
+// 2. 내가 추가로 넣고 싶은 데이터를 넣기
+
+const newTodos = filterTodos2.map((elem) => {
+  elem.isSubmit = false
+  elem.public_type = 0
+  return elem
+})
+console.log('newTodos', newTodos)
+
+
+
+
+
+// const testarr = [
+//   {users_seq : 1, todos_seq: 1, public_type: 0, filepath: "abc", isSubmit: true},
+//   {users_seq : 1, todos_seq: 2, public_type: 1, filepath: "abcd", isSubmit: false},
+//   {users_seq : 1, todos_seq: 3, public_type: 2, filepath: "abcde", isSubmit: true},
+// ]
+
+const createPayload = (arr) => {
+  arr.forEach(function(elem) {
+    const data = {
+    users_seq: elem.users_seq,
+    todos_seq: elem.seq,
+    public_type:"int",
+    filepath: "String",
+    }
+    payload.push(data)
+  })
+}
+
+createPayload(temp)
+
+seq:"Integer"
+users_seq:"Integer"
+categories_seq:"Integer",
+title:"String",
+status:"Integer",
+total_second:"String"
+
 // store에서 불러 와야 할 아이들
 const computedArticleList = computed(() => {
   return store.articles
--- modac-frontend/src/stores/article.js
+++ modac-frontend/src/stores/article.js
@@ -1,64 +1,153 @@
+import { useTodoStore } from "../../stores/todo";
 import { reactive, ref } from "vue";
 import { defineStore } from "pinia";
 import axios from "axios";
 
-export const useTodoStore = defineStore("article", () => {
+
+export const useArticleStore = defineStore("article", () => {
   const articles = reactive([]);
   const userId = ref(null);
+  const todoStore = useTodoStore()
   const backendServerUrl = ref("localhost");
+  
 
- 
-  // 게시글 조회
-  const getArticleList = () => {
-    console.log(2);
+  // 이걸로 데이터를 조작하여 원하는 데이터 형식으로 나는 axios 요청을 보낼 거임
+  const originalTodos = [{todos_seq: 1, status: 1}, {todos_seq: 1, status: 2}]
+  // const originalTodos = todostore.todos
+
+  // 1. filter 사용해서 각 객체의 status가 2(완료)인 것인 새로운 배열 만들기
+  const filterTodos = originalTodos.filter((elem) => { 
+      console.log(elem)
+      return elem.status === 2
+  })
+  // const filterTodos = todostore.todos.filter(todo => todo.status === 2);
+  
+  console.log('filterTodos', filterTodos)
+  
+  // 2. 내가 추가로 넣고 싶은 데이터를 넣기 (초기값)
+  // isSubmit : TIL 작성 완료 여부 / public_type : 공개 범위 설정
+  
+  const newTodos = filterTodos.map((elem) => {
+    elem.isSubmit = false
+    elem.public_type = 0
+    return elem
+  })
+  console.log('newTodos', newTodos)
+  
+  
+  // // 게시글 전체 조회
+  // const getArticleList = () => {
+  //   console.log('get request 실행');
+  //   console.log('게시글 전체 조회');
+  //   axios({
+  //     method: "get",
+  //     url: `${backendServerUrl.value}/article/list`,
+  //     params: {
+  //       users_seq: userId,
+  //     },
+  //   })
+  //     .then((res) => {
+  //       console.log('게시글 전체 조회 성공');
+  //       console.log('res.data: ', res.data);
+  //       articles.value = res.data;
+  //     })
+  //     .catch((err) => {
+  //       console.log('게시글 전체 조회 실패')
+  //       console.log('에러: ',err);
+  //     });
+  // };
+  // // 게시글 작성
+  // const createArticleListItem = () => {
+  //   console.log("post request 실행");
+  //   console.log('게시물 생성 요청')
+  //   axios({
+  //     methods: "post",
+  //     url: `${backendServerUrl.value}/article`,
+  //     data: {
+  //       users_seq: "int",
+  //       todos_seq: "int",
+  //       public_type: "int",
+  //       filepath: "String"
+  //     },
+  //   })
+  //     .then((res) => {
+  //       console.log('게시물 생성 성공')
+  //       console.log('res.data: ', res.data);
+  //       articles.push(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log('게시물 생성 실패');
+  //       console.log('에러: ',err);
+  //     });
+  // };
+
+  // // 게시물 조회
+  // const getArticleListItem = (articleId) => {
+  //   console.log("get request 실행");
+  //   console.log('게시물 한개 조회용');
+  //   axios({
+  //     methods: "get",
+  //     url: `${backendServerUrl.value}/article`,
+  //     params: {
+  //       seq: articleId,
+  //     }
+  //   })
+  //     .then((res) => {
+  //       console.log('게시물 조회 성공')
+  //       console.log('res.data: ', res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log('게시물 조회 실패');
+  //       console.log('에러: ',err);
+  //     });
+  // };
+  
+  // // 게시글 삭제
+  // const deleteArticleListItem = (articleId) => {
+  //   console.log("delete request 실행");
+  //   axios({
+  //     methods: "delete",
+  //     url: `${backendServerUrl.value}/todo`,
+  //     data: {
+  //       seq: articleId
+  //     },
+  //   })
+  //     .then((res) => {
+  //       console.log('게시물 삭제 성공')
+  //       console.log('res.data: ', res.data);
+  //       articles.delete(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log('게시물 삭제 실패');
+  //       console.log('에러: ',err);
+  //     });
+  // };
+
+  // Create 
+  const createArticleListItem = (todo, submitData) => {
+    console.log("post request 실행");
+    console.log('게시물 생성 요청')
     axios({
-      method: "get",
-      url: `${backendServerUrl.value}/article/list`,
-      params: {
-        users_seq: userId,
-      },
+          methods: "post",
+          url: `${backendServerUrl.value}/article`,
+          data: {
+            users_seq: userId,
+            todos_seq: todo.Id,
+            public_type: submitData.public_type,
+            filepath: submitData.filepath
+          }
     })
       .then((res) => {
-        console.log(res.data);
-        articles.value = res.data;
+        console.log('게시물 생성 성공')
+        console.log('res.data: ', res.data);
+        articles.delete(res.data);
       })
       .catch((err) => {
-        console.log(err);
+        console.log('게시물 생성 실패');
+        console.log('에러: ',err);
       });
-  };
-  // 게시글 작성
-  const createArticleListItem = () => {
-    console.log("post request 실행");
-    axios({
-      methods: "post",
-      url: `${backendServerUrl.value}/article`,
-      data: {
-        users_seq:"int",
-        todos_seq:"int",
-        public_type:"int",
-        filepath: "String"
-      },
-    });
-  };
+    }
 
-  // 게시글 삭제
-  const deleteArticleListItem = () => {
-    console.log("delete request 실행");
-    axios({
-      methods: "delete",
-      url: `${backendServerUrl.value}/todo`,
-      data: {},
-    });
-  };
-  // 게시글 수정
-  const changeArticleListItem = () => {
-    console.log("put request 실행");
-    axios({
-      methods: "put",
-      url: `${backendServerUrl.value}/todo`,
-      data: {},
-    });
-  };
 
   // 백이랑 연결 되면 삭제 예정
   const addArticleItem = (inputarticleItem) => {
@@ -70,8 +159,10 @@ export const useTodoStore = defineStore("article", () => {
 
   return {
     articles,
-    addArticleItem,
-    getArticleList,
+    // getArticleList,
     createArticleListItem,
+    // getArticleListItem,
+    // deleteArticleListItem,
+    addArticleItem,
   };
 });
