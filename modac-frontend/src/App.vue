4e43fba2a5441805359eb8fb2958ee0edb8bf4a2
권용재 <yongjae116@gmail.com>
2023-01-20 15:56:27 +0900

    Feat: Feat: Navbar 생성
    네비게이션바 그냥 갖다 붙여놨음

--- modac-frontend/src/App.vue
+++ modac-frontend/src/App.vue
@@ -1,15 +1,12 @@
-<script setup></script>
+<script setup>
+import NavbarTop from './components/NavbarTop.vue';
+</script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/todoList">투두 리스트</RouterLink><br />
-      <RouterLink to="/studyRoom">스터디 룸</RouterLink><br />
-      <RouterLink to="/before">깃헙 연동 테스트</RouterLink>
-    </nav>
-  </header>
-
-  <RouterView />
+  <div>
+    <NavbarTop/>
+  </div>
+  <RouterView/>
 </template>
 
 <style scoped></style>
---
+++ modac-frontend/src/components/NavbarTop.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="navigation">
+            <nav>
+              <ul class="nav-type">
+                <RouterLink id="routerlink" to="/todoList">투두 리스트</RouterLink>
+                <RouterLink id="routerlink" to="/studyRoom">스터디 룸</RouterLink>
+                <RouterLink id="routerlink" to="/before">깃헙 연동 테스트</RouterLink>
+              </ul>
+            </nav>
+          </div>
+</template>
+
+<script setup>
+function myData() {
+  retrun;
+}
+
+function show() {
+  document.getElementById('anotherFunction').classList.toggle('Active');
+}
+</script>
+
+<style>
+@import '@/components/NavbarTop.css'
+</style>
