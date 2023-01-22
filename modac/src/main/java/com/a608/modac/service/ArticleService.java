591774274984294dcbe85950048bd665e7e241c3
hiflo <xswaqz@naver.com>
2023-01-22 15:37:19 +0900

    Fix: Article 관련 기능 오류 수정 Request DTO, Response DTO의 역할이 명확하지 않았고, 틀린 코드도 존재하여 수정하였음. 그에 따라 Controller와 Service, ServiceImpl 파일 또한 수정함.

--- modac/src/main/java/com/a608/modac/service/ArticleService.java
+++ modac/src/main/java/com/a608/modac/service/ArticleService.java
@@ -4,17 +4,18 @@ import java.util.List;
 
 import com.a608.modac.model.article.Article;
 import com.a608.modac.model.article.ArticleRequest;
+import com.a608.modac.model.article.ArticleResponse;
 import com.a608.modac.model.todo.TodoResponse;
 
 public interface ArticleService {
-	// 게시글 등록
-	void createArticle(ArticleRequest.createArticleRequest registerDto, TodoResponse todoResponse);
+	// 게시글 저장
+	void createArticle(ArticleRequest articleRequest);
 
 	// 사용자 아이디로 게시글 조회
-	List<Article> readArticleByUsersSeq(Long usersSeq);
+	List<ArticleResponse> readArticleByUsersSeq(Long usersSeq);
 
 	// 게시글 번호로 게시글 조회
-	Article readArticleBySeq(Long seq);
+	ArticleResponse readArticleBySeq(Long seq);
 
 	// 게시글 번호로 게시글 삭제
 	void deleteArticleBySeq(Long seq);
--- modac/src/main/java/com/a608/modac/service/ArticleServiceImpl.java
+++ modac/src/main/java/com/a608/modac/service/ArticleServiceImpl.java
@@ -1,10 +1,9 @@
 package com.a608.modac.service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 
@@ -13,52 +12,48 @@ import org.springframework.stereotype.Service;
 import com.a608.modac.model.article.Article;
 import com.a608.modac.model.article.ArticleRepository;
 import com.a608.modac.model.article.ArticleRequest;
+import com.a608.modac.model.article.ArticleResponse;
+import com.a608.modac.model.todo.Todo;
 import com.a608.modac.model.todo.TodoRepository;
-import com.a608.modac.model.todo.TodoResponse;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
 
-	@Resource(name="articleRepository")
-	private ArticleRepository articleRepository;
+	@Resource(name = "articleRepository")
+	private final ArticleRepository articleRepository;
 
-	@Resource(name="todoRepository")
-	private TodoRepository todoRepository;
+	@Resource(name = "todoRepository")
+	private final TodoRepository todoRepository;
 
-
-
-	@Override
-	public void createArticle(ArticleRequest.createArticleRequest registerDto, TodoResponse todoResponse) {
-		// Article 객체에 들어갈 요소들 정리
-		String title = todoResponse.getTitle();
-		String filepath = registerDto.getFilepath();
-		String registeredTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-		int publicType = registerDto.getPublicType();
-		String totalSecond = todoResponse.getTotalSecond();
-		Long usersSeq = registerDto.getUsersSeq();
-		Long categoriesSeq = todoResponse.getCategoriesSeq();
-
-		// Article 객체 생성
-		Article article = new Article(null, title, filepath, registeredTime, publicType,
-			 0, 0, 0, totalSecond, usersSeq, categoriesSeq);
-
-		// Article 객체 저장
-		articleRepository.save(article);
+	public ArticleServiceImpl(ArticleRepository articleRepository, TodoRepository todoRepository) {
+		this.articleRepository = articleRepository;
+		this.todoRepository = todoRepository;
 	}
 
+	// 게시글 저장
 	@Override
-	public List<Article> readArticleByUsersSeq(Long usersSeq) {
-		return articleRepository.findByUsersSeq(usersSeq);
+	public void createArticle(final ArticleRequest articleRequest) {
+		Todo todo = todoRepository.findById(articleRequest.getTodosSeq())
+			.orElseThrow(NoSuchElementException::new); // todosSeq를 이용하여 todo 호출
+		articleRepository.save(articleRequest.toEntity(todo)); // Article 빌드 후 저장
 	}
 
+	// 사용자 아이디로 게시글 조회
 	@Override
-	public Article readArticleBySeq(Long seq) {
-		Optional<Article> article = articleRepository.findById(seq);
-		return article.orElseThrow(() -> new NoSuchElementException("Article not found."));
+	public List<ArticleResponse> readArticleByUsersSeq(final Long usersSeq) {
+		final List<Article> findArticles = articleRepository.findByUsersSeq(usersSeq);
+		return findArticles.stream().map(ArticleResponse::new).collect(Collectors.toList());
 	}
 
+	// 게시글 번호로 게시글 조회
 	@Override
-	public void deleteArticleBySeq(Long seq) {
+	public ArticleResponse readArticleBySeq(final Long seq) {
+		return new ArticleResponse(articleRepository.findById(seq).orElseThrow(NoSuchElementException::new));
+	}
+
+	// 게시글 번호로 게시글 삭제
+	@Override
+	public void deleteArticleBySeq(final Long seq) {
 		articleRepository.deleteById(seq);
 	}
 }
