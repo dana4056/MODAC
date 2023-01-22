7b627c7fb24249c96529b8db10d59c86b886aaba
hiflo <xswaqz@naver.com>
2023-01-22 16:24:17 +0900

    Feat: Comment Service 구현 Comment 관련 Service, ServiceImpl 기능을 작성하고, Comment Repository에서 JpaRepository를 상속하는 코드를 추가함

--- modac/src/main/java/com/a608/modac/model/comment/CommentRepository.java
+++ modac/src/main/java/com/a608/modac/model/comment/CommentRepository.java
@@ -2,12 +2,13 @@ package com.a608.modac.model.comment;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	// articlesSeq를 전달받아 일치하는 댓글을 리스트에 담아 반환
-	List<Comment> findByArticlesSeq(Long articlesSeq);
+	List<Comment> findCommentsByArticlesSeq(final Long articlesSeq);
 
 }
--- modac/src/main/java/com/a608/modac/service/CommentServiceImpl.java
+++ modac/src/main/java/com/a608/modac/service/CommentServiceImpl.java
@@ -1,7 +1,39 @@
 package com.a608.modac.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import com.a608.modac.model.comment.Comment;
+import com.a608.modac.model.comment.CommentRepository;
+import com.a608.modac.model.comment.CommentRequest;
+import com.a608.modac.model.comment.CommentResponse;
+
 @Service
-public class CommentServiceImpl implements CommentService{
+public class CommentServiceImpl implements CommentService {
+	private CommentRepository commentRepository;
+
+	public CommentServiceImpl(CommentRepository commentRepository) {
+		this.commentRepository = commentRepository;
+	}
+
+	// 댓글 작성
+	@Override
+	public void createComment(final CommentRequest commentRequest) {
+		commentRepository.save(commentRequest.toEntity());
+	}
+
+	// 댓글 삭제
+	@Override
+	public void deleteComment(final Long seq) {
+		commentRepository.deleteById(seq);
+	}
+
+	// 댓글 목록조회
+	@Override
+	public List<CommentResponse> readCommentByArticlesSeq(final Long articlesSeq) {
+		final List<Comment> findComment = commentRepository.findCommentsByArticlesSeq(articlesSeq);
+		return findComment.stream().map(CommentResponse::new).collect(Collectors.toList());
+	}
 }
