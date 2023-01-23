c912dfa6ef3c8895896da798ce36f8199ce528c5
권용재 <yongjae116@gmail.com>
2023-01-24 03:56:04 +0900

    Feat: 로그인화면 css 초안
    팀원들과 회의 후에 추후 수정 예정

--- modac-frontend/src/domains/User/Login.vue
+++ modac-frontend/src/domains/User/Login.vue
@@ -26,6 +26,26 @@
     </ul>
   </div>
 </div>
+
+<div class="z-3">
+  <div class="tile top-left animate-opacity freq-5"></div>
+  <div class="tile top-right animate-opacity freq-9"></div>
+  <div class="tile bottom-left animate-opacity freq-7"></div>
+  <div class="tile bottom-right animate-opacity freq-10"></div>
+</div>
+<div class="z-2">
+  <div class="tile top-left animate-opacity freq-9 delay-2"></div>
+  <div class="tile top-right animate-opacity freq-5 delay-2"></div>
+  <div class="tile bottom-left animate-opacity freq-6 delay-4"></div>
+  <div class="tile bottom-right animate-opacity freq-10 delay-4"></div>
+</div>
+<div class="z-1">
+  <div class="tile top-left animate-opacity freq-7 delay-2"></div>
+  <div class="tile top-right animate-opacity freq-5 delay-4"></div>
+  <div class="tile bottom-left animate-opacity freq-9 delay-2"></div>
+  <div class="tile bottom-right animate-opacity freq-5 delay"></div>
+</div>
+
 </template>
 
 <script>
