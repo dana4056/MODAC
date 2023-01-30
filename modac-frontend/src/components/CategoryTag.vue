4658ed827ac3d933a67b684b537159f482e75ecc
benkim077 <123vfcx@gmail.com>
2023-01-30 17:54:11 +0900

    Feat: CategoryTag 구현

---
+++ modac-frontend/src/components/CategoryTag.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, defineProps, computed, onMounted } from "vue";
+
+onMounted(() => {
+  const slotTag = document.querySelector("#slot");
+  console.log(slotTag);
+  const slotTagInnerText = ref(slotTag.innerText);
+});
+
+const categoryStyleMap = {
+  algorithm: "study_category_style_algorithm",
+  cs: "study_category_style_cs",
+  develop: "study_category_style_develop",
+  planning: "study_category_style_planning",
+  etc: "study_category_style_etc",
+};
+
+const props = defineProps({
+  categoryName: String,
+});
+
+// const categoryStyleState = computed(() => {
+//   return categoryStyleMap[props.categoryName];
+// });
+
+const categoryStyleState = computed(() => {
+  return categoryStyleMap.slotTagInnerText.value;
+});
+</script>
+
+<template>
+  <span :class="$style[categoryStyleState]">
+    <slot id="slot"></slot>
+  </span>
+</template>
+
+<style lang="css" module>
+@import "./CategoryTag.module.css";
+</style>
--- modac-frontend/src/domains/Room/RoomList/RoomListHeader.vue
+++ modac-frontend/src/domains/Room/RoomList/RoomListHeader.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import CategoryTag from "@/components/CategoryTag.vue";
+</script>
 
 <template>
   <h1>멀티룸 리스트</h1>
+  <CategoryTag>CS</CategoryTag>
   <button>멀티룸 생성하기 -></button>
   <input type="text" placeholder="원하는 방을 검색해보세요." />
 </template>
