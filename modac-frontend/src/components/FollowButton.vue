24f48232b3f1e39dffe0d92e400b4f24b07e7e98
권용재 <yongjae116@gmail.com>
2023-01-31 14:51:42 +0900

    Feat: Like/Follow 버튼 만들다 맘
    뮤직 플레이어 처리하고 다시 돌아온다..

---
+++ modac-frontend/src/components/FollowButton.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <button :class="$style.btn_flip">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="css" module>
+@import "./FollowButton.module.css";
+</style>
