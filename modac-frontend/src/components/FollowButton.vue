79ce587c7c7bbc03fee092bcde2c7ef4ed73f48c
권용재 <yongjae116@gmail.com>
2023-02-01 13:27:06 +0900

    Feat: Follow Transition Button 생성
    집에가서 완성 할 예정

--- modac-frontend/src/components/FollowButton.vue
+++ modac-frontend/src/components/FollowButton.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+let is_followed = ref(false)
+const follow = () => {
+  is_followed.value ? is_followed.value = false : is_followed.value = true
+}
+</script>
 
 <template>
-  <button :class="$style.btn_flip">
+  <button :class="$style.btn_flip" data-back="follow" data-front="Unfollow">
     <slot></slot>
   </button>
 </template>
