1cbdb268470e108b954464e93af7dc89e3e828fb
권용재 <yongjae116@gmail.com>
2023-02-02 10:57:18 +0900

    Feat: feed 화면 레이아웃
    화면 비율 이상해서 잠시 임시 커밋

---
+++ modac-frontend/src/domains/Feed/FeedList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 class="my_color my_size">나는 피드 아이템</h1>
+    <FeedListItem
+        :class="$style.feed_item"
+        v-for="(feed, index) in computedFeedList"
+        :key="index"
+        :feed="feed"
+        />
+  </div>
+</template>
+
+<script setup>
+import FeedListItem from "./FeedListItem.vue"
+import { useFeedStore } from "../../stores/feed";
+import { computed } from "vue"
+const store = useFeedStore()
+
+
+
+// store에서 불러 와야 할 아이들
+const computedFeedList = computed(() => {
+  return store.feeds
+})
+
+// 화면 생성 시 실행되어야 할 함수
+store.getFeeds()
+
+</script>
+
+<style lang="css" module>
+@import "./FeedList.module.css"
+
+</style>
---
+++ modac-frontend/src/domains/Feed/FeedListItem.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1 class="temp1">게시글 {{ feed.feed_seq }} 번</h1>
+		{{ feed }}
+		<button
+		id="delete"
+		>X</button>
+	</div>
+</template>
+
+<script setup>
+import { useFeedStore } from "../../stores/feed";
+defineProps(['feed'])
+const store = useFeedStore()
+
+
+</script>
+
+<style>
+@import './FeedListItem.module.css'
+</style>
