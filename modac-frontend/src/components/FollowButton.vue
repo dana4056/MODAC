d43035317113ce8c1631ebb4c53bd5d01575ce11
권용재 <yongjae116@gmail.com>
2023-02-03 14:08:20 +0900

    Feat: Follow / 좋아요 버튼 생성 완료
    기본 틀은 작성 했으나 모양 자체는 좀 수정 해야할 필요 있음

--- modac-frontend/src/components/FollowButton.vue
+++ modac-frontend/src/components/FollowButton.vue
@@ -1,36 +1,46 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { computed, ref, onMounted, useCssModule } from 'vue'
+
 let is_followed = ref(false)
 let followbtn = null
+const button_content = computed(() => is_followed.value ? '언팔' : '팔로')
+
 
 const followOrUnfollow = () => {
+  console.log(is_followed.value);
   is_followed.value ? unfollow(): follow()
 }
 
+const $style = useCssModule()
 
 const follow = () => {
+  console.log('1');
   is_followed.value = true
-  followbtn.classList.add('button_unfollow')
-  followbtn.classList.remove('button_follow')
+  followbtn.classList.remove($style.follow_to_unfollow)
+  followbtn.classList.add($style.unfollow_to_follow)
 }
 
 const unfollow = () => {
+  console.log('2');
   is_followed.value = false
-  followbtn.classList.remove('button_unfollow')
-  followbtn.classList.add('button_follow')
+  followbtn.classList.add($style.follow_to_unfollow)
+  followbtn.classList.remove($style.unfollow_to_follow)
 }
 
 onMounted(() => {
   followbtn = document.querySelector('#follow_btn')
 })
-
 </script>
 
 <template>
-  <button id="follow_btn" class="button_follow" data-follow="팔로우" data-unfollow="팔로잉" @click="followOrUnfollow">
-  </button>
+<button id="follow_btn"
+  :class="$style.follow_to_unfollow"
+  @click="followOrUnfollow">
+  {{ button_content  }}
+</button>
+
 </template>
 
-<style lang="css">
-@import "./FollowButton.module.css";
+<style lang="css" module>
+@import "FollowButton.module.css"
 </style>
--- modac-frontend/src/views/TestView.vue
+++ modac-frontend/src/views/TestView.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h1>여기는 TestView 입니다.</h1>
-    <FollowButton/>
+    <!-- <FollowButton/> -->
+    <LikeButtonVue/>
   </div>
 </template>
 <script setup>
 import FollowButton from '@/components/FollowButton.vue';
+import LikeButtonVue from '@/domains/Feed/LikeButton.vue';
+
 </script>
 <style lang="css"></style>
