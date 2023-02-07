1ee5a00773243d5cf9710edd40a36896138dcf16
dhkim <xswaqz@naver.com>
2023-02-07 16:08:36 +0900

    Feat: 게시글, 댓글 API 테스트 완료 명세서에 있는 게시글, 댓글 관련 API를 api폴더 안의 js파일에 구현 및 테스트 완료

--- modac-frontend/src/views/ApiTestView.vue
+++ modac-frontend/src/views/ApiTestView.vue
@@ -8,9 +8,12 @@
 <script setup>
 
 import { useUserStore } from '../stores/user';
+import { useArticleStore } from '../stores/article2';
+import { useCommentStore } from '../stores/comment';
 
 const store = useUserStore();
-
+const articleStore = useArticleStore();
+const commentStore = useCommentStore();
 
 //============================= API 파라미터 데이터 ==========================
 const data1 = {     // 회원가입 데이터
@@ -43,14 +46,46 @@ const data5 = {   // 로그인 데이터
   password: "vue2"
 }
 
+// ================================
+
+const data6 = {   // 게시글 작성 데이터
+  usersSeq: 1,
+  todosSeq: 1,
+  publicType: 1,
+  filepath: "# 이거는 게시글 내용이다"
+};
+
+const data7 = {   // 사용자 게시글, 팔로잉 기반 게시글 조회 데이터
+  usersSeq: 1,
+  offset: 1,
+  limit: 10
+};
+
+const data8 = {   // 게시글 좋아요 관련 데이터
+  usersSeq: 2,
+  articlesSeq: 2
+};
+
+// ======================================
+
+const data9  = {  // 댓글
+  articlesSeq: 2,
+  usersSeq: 1,
+  content: "소통해요~~~1"
+}
+
+
+// ======================================
+
+
 
 //==========================================================================
 
 function click(){
 
   /********** 파라미터가 객체형식인 것은 위에 정리 / 아닌건 그냥 기입 *************/
-  store.api.postUser(data1);                   // 회원가입
-  store.api.fetchUser(2);                      // 회원정보 조회
+  // store.api.postUser(data1);                   // 회원가입
+  // store.api.fetchUser(2);                      // 회원정보 조회
   // store.api.updateUser(data2);                 // 회원정보 수정
   // store.api.updatePW(data3);                   // 비밀번호 수정
   // store.api.updatePoint(data4);                // 포인트 수정
@@ -61,6 +96,21 @@ function click(){
   // store.api.checkUserId("test1");              // 아이디 중복확인
   // store.api.checkUserEmail("vue1@gmail.com");  // 이메일 중복확인
   // store.api.login(data5);                      // 로그인
+  
+  // articleStore.api.postArticle(data1);         // 게시글 작성
+  // articleStore.api.findArticleByUsersSeq(data2);  // 사용자 게시글 전체 조회
+  // articleStore.api.findArticle(1);             // 게시글 조회
+  // articleStore.api.updateViewCount(1);         // 게시글 조회수 올리기
+  // articleStore.api.deleteArticle(1);           // 게시글 삭제
+  // articleStore.api.findArticleByFollowing(data3); // 팔로잉 기반 게시글 조회
+  // articleStore.api.likeArticle(data4);         // 게시글 좋아요
+  // articleStore.api.unlikeArticle(data4);       // 게시글 좋아요 취소
+  // articleStore.api.isLikedArticle(data4);      // 게시글 좋아요 관계정보 조회
+
+  // commentStore.api.postComment(data9);         // 댓글 작성
+  // commentStore.api.findCommentList(2);         // 게시글별 댓글 목록 조회
+  // commentStore.api.deleteComment(2);           // 댓글 삭제
+
 
 }
 
