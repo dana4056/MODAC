b4c16bc13a740dea5de8be9b099a4a11cc917f48
권용재 <koj185364@naver.com>
2023-02-07 15:44:48 +0900

    Update TestView.vue

--- modac-frontend/src/views/TestView.vue
+++ modac-frontend/src/views/TestView.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <h1>여기는 TestView 입니다.</h1>
-    <!-- <FollowButton/> -->
-    <LikeButtonVue/>
+
   </div>
 </template>
 <script setup>
-import FollowButton from '@/components/FollowButton.vue';
-import LikeButtonVue from '@/domains/Feed/LikeButton.vue';
+
 
 </script>
 <style lang="css"></style>
