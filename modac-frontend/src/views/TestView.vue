ba199e964c257aa640dfa4d4a3054f2f33dd98cd
권용재 <yongjae116@gmail.com>
2023-02-07 15:48:56 +0900

    Refactor: TestView Clear
    테스트 뷰에 필요 없는 부분 제거

--- modac-frontend/src/views/TestView.vue
+++ modac-frontend/src/views/TestView.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
     <h1>여기는 TestView 입니다.</h1>
-    <Notification />
   </div>
 </template>
 <script setup>
-import Notification from '@/components/Notification.vue';
 
 </script>
 <style lang="css"></style>
