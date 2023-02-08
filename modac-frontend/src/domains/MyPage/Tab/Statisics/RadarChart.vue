f710db0e844e6ee52924576bc15ce300682596bd
권용재 <yongjae116@gmail.com>
2023-02-08 15:45:45 +0900

    Feat: 통계 생성
    Bar 통계 작성 / Radar 통계 작성

---
+++ modac-frontend/src/domains/MyPage/Tab/Statisics/RadarChart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <canvas id="radar-chart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+import radarChartData from './radarchart-data.js'
+
+export default {
+  name: 'radarChart',
+  data(){
+    return {
+      radarChartData: radarChartData
+    }
+  },
+  mounted(){
+    const ctx = document.getElementById('radar-chart');
+    new Chart(ctx, this.radarChartData)
+  }
+}
+</script>
