f9d8555391bd06a79588b7224f114418f90c84e4
benkim077 <123vfcx@gmail.com>
2023-02-13 21:56:52 +0900

    Feat: updateTodo, deleteTodo 기능 구현

---
+++ modac-frontend/src/domains/Room/Todo/PlayPauseButton.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  status: Number,
+});
+</script>
+
+<template>
+  <button v-if="props.status === 1" @click="playPauseTodoItem">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="w-5 h-5"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </button>
+  <button v-else @click="playPauseTodoItem">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="w-5 h-5"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </button>
+</template>
+
+<style lang="css" module>
+@import "PlayPauseButton.module.css";
+</style>
---
+++ modac-frontend/src/domains/Room/Todo/TodoItemContent.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  openState: Boolean,
+  status: Number,
+  title: String,
+});
+</script>
+
+<template>
+  <div
+    v-if="props.openState && props.status == 2"
+    :class="$style.style1"
+    @click="toggleOpenState"
+  >
+    {{ props.title }}
+  </div>
+  <div
+    v-if="props.openState && props.status != 2"
+    :class="$style.style2"
+    @click="toggleOpenState"
+  >
+    {{ props.title }}
+  </div>
+  <div v-if="!props.openState" @click="toggleOpenState" :class="$style.style3">
+    {{ props.title }}
+  </div>
+</template>
+
+<style lang="css" module>
+@import "TodoItemContent.module.css";
+</style>
