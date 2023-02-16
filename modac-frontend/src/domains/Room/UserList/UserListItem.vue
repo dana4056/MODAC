6018c15d77207246f4a5855b208494eae961fc80
benkim077 <123vfcx@gmail.com>
2023-02-16 20:40:16 +0900

    Design: 룸 메인섹션 디자인 변경
    - 고양이 배치 및 크기
    - 드롭다운 생성

--- modac-frontend/src/domains/Room/UserList/UserListItem.vue
+++ modac-frontend/src/domains/Room/UserList/UserListItem.vue
@@ -1,20 +1,53 @@
 <script setup>
 import Card from "@/components/Card.vue";
 import CardTitle from "@/components/CardTitle.vue";
-import UserStatusIndicator from "@/components/UserStatusIndicator.vue";
-import { defineProps, useCssModule } from "vue";
+// import UserStatusIndicator from "@/components/UserStatusIndicator.vue";
+import { ref, defineProps, useCssModule } from "vue";
 
 const $style = useCssModule();
 
 const props = defineProps({
   userItem: Object,
 });
+
+const isDropdownOpenState = ref(false);
+const openDropdown = () => {
+  isDropdownOpenState.value = true;
+};
+const closeDropdown = () => {
+  isDropdownOpenState.value = false;
+};
+const clickDropdownButtonHandler = () => {
+  isDropdownOpenState.value ? closeDropdown() : openDropdown();
+};
+const blurDropdownButtonHandler = () => {
+  if (isDropdownOpenState.value === true) {
+    closeDropdown();
+  }
+};
 </script>
 
 <template>
   <div>
     <div :class="$style.card_style">
-      <div style="text-align: center">
+      <div>
+        <div
+          v-if="isDropdownOpenState"
+          id="dropdown"
+          :class="$style.dropdown"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="dropdown_button"
+          tabindex="-1"
+        >
+          <!-- 여기에 내용을 넣으세요  -->
+        </div>
+      </div>
+      <div
+        :class="$style.user_card_button"
+        @click="clickDropdownButtonHandler"
+        @blur="blurDropdownButtonHandler"
+      >
         <CardTitle :class="$style.display_inline_block">{{
           props.userItem.nickname
         }}</CardTitle>
